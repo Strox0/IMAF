41984a5cccea468eb92da841738fdc285fb502ea
Strox0 <molnartamas332@gmail.com>
2023-07-27 15:41:16 +0200

    updated premake scripts

--- IMAF_setup.lua
+++ IMAF_setup.lua
@@ -8,51 +8,7 @@ workspace "IMAF"
     buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
 include "vendor/glfw"
-
-project "IMAF"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++20"
-	staticruntime "off"
-	location "IMAF"
-
-	files {
-		"IMAF/src/**.cpp",
-		"IMAF/src/**.h",
-		"vendor/imgui/**.cpp",
-		"vendor/imgui/**.h",
-		"vendor/opengl/api/**.h"
-	}
-
-	includedirs {
-		"IMAF/src",
-		"vendor/imgui",
-		"vendor/glfw/include",
-		"vendor/opengl/api",
-		"vendor/KHR"
-	}
-
-	links {
-		"glfw",
-		"opengl32"
-	}
-
-	targetdir ("IMAF/bin/" .. outputdir .. "/")
-	objdir ("IMAF/intermediate/" .. outputdir .. "/")
-
-	filter "system:windows"
-      systemversion "latest"
-
-	filter "configurations:Debug"
-      defines { "_DEBUG" }
-      runtime "Debug"
-      symbols "On"
-
-	filter "configurations:Release"
-      defines { "NDEBUG" }
-      runtime "Release"
-      optimize "On"
-      symbols "Off"
+include "IMAF"
 
 project "Test_App"
 	kind "WindowedApp"
