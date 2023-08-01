b7a840b4e6ed6d13ff221093d485ecd4d4a5b76a
Strox0 <molnartamas332@gmail.com>
2023-08-01 18:31:02 +0200

    test code update, new imgui window

--- Test_App/src/Main.cpp
+++ Test_App/src/Main.cpp
@@ -1,15 +1,33 @@
 #include "Application.h"
+#include "Panel.h"
 #include <windows.h>
+
+class ExampleLayer : public IMAF::Panel
+{
+public:
+	void UiRender() override
+	{
+		ImGui::Begin("Hello");
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+
+		ImGui::ShowDemoWindow();
+	}
+};
 
 int WINAPI wWinMain(HINSTANCE hInstance,HINSTANCE prevhInstance,PWSTR pCmdLine,int cmdShow)
 {
 	IMAF::AppProperties props;
-	props.costum_titlebar = false;
-	props.costum_titlebar_area = 0.07f;
+	props.resizeable = true;
+	props.imgui_docking = true;
 
 	IMAF::Application* app = new IMAF::Application(props);
-
+	std::shared_ptr<ExampleLayer> ptr = std::make_shared<ExampleLayer>();
+	app->AddPanel(ptr);
+	
 	app->Run();
+
+	delete app;
 
 	return 0;
 }
